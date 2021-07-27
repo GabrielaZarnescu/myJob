1a9a252ad47d8143f223128cc52e4eb2815a6cc4
GabrielaZarnescu <zarnescu_gabriela@yahoo.com>
2021-07-27 16:19:16 +0300

    created navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import Navbar from './components/Navbar'
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      <Navbar/>
     </div>
   );
 }
---
+++ client/src/components/MenuItems.js
@@ -0,0 +1,23 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Posts',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Contact',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Sign in',
+        url: '#',
+        cName: 'nav-links-mobile',
+    },
+    
+]
