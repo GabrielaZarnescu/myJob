26a4c876628db2eb4f057e4176716f12b8e4904a
GabrielaZarnescu <zarnescu_gabriela@yahoo.com>
2021-08-05 20:58:00 +0300

    solved styling problem

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,47 +1,51 @@
 import React, { Component } from 'react'
 //import { MenuItems } from './MenuItems';
-import {Button} from '../Button/Button';
-import {Link} from 'react-router-dom'
+import { Button } from '../Button/Button';
+import { Link } from 'react-router-dom'
 import './Navbar.css';
-import{
+import {
     Collapse,
-  NavbarToggler,
-  Nav,
-  NavItem,
-  Container,
+    NavbarToggler,
+    Nav,
+    NavItem,
+    Container,
 } from 'reactstrap';
 export class Navbar extends Component {
     state = { clicked: false }
-    handleClick=()=> {
-        this.setState({clicked: !this.state.clicked})
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
     }
-    
+
     render() {
         const size2 = {
             height: 130,
-            width: 250
-        } ;
+            width: 250,
+            position: 'unset'
+        };
         const size = {
-            height:200, width:400
-        } ; 
+            height: 200, width: 400
+        };
         return (
             <nav className="NavbarItems">
-                <img classname="navbar-logo"  src={`${process.env.PUBLIC_URL}/assets/images/logoo.png`}></img>
-                <div className="menu-icon" onClick={this.handleClick}>
-                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                <img className="navlogo" style={size2} src={`${process.env.PUBLIC_URL}/assets/images/logoo.png`}></img>
+                <div className='menu-group'>
+                    <div className="menu-icon" onClick={this.handleClick}>
+                        <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                    </div>
+
+                    <Nav className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                        <Link to="/" className="nav-links">
+                            <NavItem >Home</NavItem></Link>
+                        <Link to="/offer" className="nav-links">
+                            <NavItem >Make a post</NavItem></Link>
+                        <Link to="/contact" className="nav-links">
+                            <NavItem >Contact</NavItem></Link>
+                        <Link to="/register" className="nav-links-mobile">
+                            <NavItem >Sign in</NavItem></Link>
+                    </Nav>
+                    <Link className="sign-btn" to="/register">
+                        <Button >Sign in</Button></Link>
                 </div>
-                <Nav className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-                    <Link to="/" className="nav-links">
-                    <NavItem >Home</NavItem></Link>
-                    <Link to="/offer" className="nav-links">
-                    <NavItem >Make a post</NavItem></Link>
-                    <Link to="/contact" className="nav-links">
-                    <NavItem >Contact</NavItem></Link>
-                    <Link to="/register" className="nav-links-mobile">
-                    <NavItem >Sign in</NavItem></Link>
-                </Nav>
-                <Link to="/register">
-                <Button>Sign in</Button></Link>
             </nav>
         )
     }
