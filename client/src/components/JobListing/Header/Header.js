1626b776ac25682055e37b290fcb5d880de2cdca
superior311 <melintewalter@gmail.com>
2021-08-11 18:37:41 +0300

    fixed

--- client/src/components/JobListing/Header/Header.js
+++ client/src/components/JobListing/Header/Header.js
@@ -2,16 +2,14 @@ import React from 'react';
 import { Box, Grid, Typography,Button } from '@material-ui/core';
 
 export default (props)=>(
- <Box py={5} bgcolor='secondary.main;' color='white'>
+ <Box py={5} bgcolor='secondary.main' color='white'>
      <Grid container justify='center'>
          <Grid item xs={10}>
             <Box display='flex' justifyContent="space-between">
-    <Typography variant='h4'> Open job listing
-
-    </Typography>
+    <Typography variant='h4'> Open job listing </Typography>
     <Button variant='contained' color='primary' disableElevation>Post a job</Button>
-    </Box>
+      </Box>
     </Grid>
-</Grid> 
-</Box>
+   </Grid> 
+   </Box>
 );
